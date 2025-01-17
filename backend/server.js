ea3841318aad7556e758c5e23d7f1672b49654a6
sounabh <152299664+sounabh@users.noreply.github.com>
2025-01-17 11:51:02 +0530

    Update server.js domain socket

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ const port = process.env.PORT || 8000;
 // Initialize Socket.IO with CORS configuration
 const io = new Server(server, {
   cors: {
-    origin: "12angrymen.vercel.app", // Allow requests from this origin (frontend URL)
+    origin: "https://12angrymen.vercel.app/", // Allow requests from this origin (frontend URL)
     methods: ["GET", "POST"],        // Allow only GET and POST HTTP methods
     credentials: true,               // Enable credentials (e.g., cookies or headers)
   },
